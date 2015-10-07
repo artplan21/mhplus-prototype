88d6c54c02ffd83e271464f2cebe52bf2de2a250
Olaf Kolbig <olaf.kolbig@gmx.de>
2015-10-07 22:55:29 +0200

    breadcrumb changed, responsive menue with 3 levels

--- Resources/Public/Scripts/js/app.js
+++ Resources/Public/Scripts/js/app.js
@@ -12,7 +12,9 @@ jQuery.noConflict();
 			close_on_click : false
 		},
 		topbar: {
+			mobile_show_parent_link: false,
 			back_text: 'Übersicht Menü', // Define what you want your custom back text to be if custom_back_text: true
+			//custom_back_text: false,
 		},
 		equalizer : {
 			// Specify if Equalizer should make elements equal height once they become stacked.
@@ -32,7 +34,7 @@ jQuery.noConflict();
 			return false;
 		}
 		
-		/* toggle searchinput in mobile */
+		/* toggle searchinput in mobile view */
 		$('#searchinput form button').click(function(event) {		
 			var $target = $('#searchinput form input');
 			console.log($target.val());
@@ -59,12 +61,53 @@ jQuery.noConflict();
 			}
 		}
 		
-		
-		
 		$('#mainnav').click(function() {
 			if ($(this).hasClass('expanded') && isMobile()) setMainnavHeight();
 		});
+	
+	
+		function createTopbarBacklink() {
+			/* create back link for topbar previous level */
+			$('#mainnav ul.dropdown').each(function() {
+				if ($(this).find('a.parent-link').length == 0) {
+					var li = $('<li>')
+						.addClass('back')
+						.addClass('js-generated');
+						
+					var a = $('<a>')
+						.attr('href', 'javascript:void(0)')
+						.html($(this).prev('a').html())
+						.addClass('parent-link')
+						.appendTo(li);
+					
+					if ($(this).children('li.title.back').length != 0) li.insertAfter($(this).children('li:first'));
+					else li.prependTo($(this));
+				}
+			});
+		}
 		
+		function createDropdownClasses() {
+			$('.top-bar-section ul').children('li').each(function(){
+				if ($(this).children('ul:not(.dropdown)').length != 0) {
+					$(this).addClass('has-dropdown');
+					$(this).children('ul').addClass('dropdown');
+				}
+			});
+		}
+		
+		function destroyDropdownClasses() {
+			$('.top-bar-section ul ul li.has-dropdown').each(function() {
+				$(this).removeClass('has-dropdown');
+				$(this).children('ul.dropdown').removeClass('dropdown');
+			});
+		}
+		
+		function overwriteTopbarBackhandler() {
+			/* overwrite back handler from topbar-back-link */
+			$('#mainnav li.title.back').addClass('fullback').find('a').unbind('click').click(function() {
+				$(this).parents('ul.dropdown').last().children('li.title.back').find('a').not($(this)).click();
+			});
+		}		
 		
 		/* create off-canvas menue toggle */
 		$('#toggle-topbar').click(function() {
@@ -76,6 +119,15 @@ jQuery.noConflict();
 			// some more
 			if($('#mainnav.expanded').length == 0) {
 			
+				// destroy foundation dropdowns
+				destroyDropdownClasses();
+				$(document).foundation({
+					topbar: {
+						mobile_show_parent_link: false,
+						back_text: 'Übersicht Menü', // Define what you want your custom back text to be if custom_back_text: true
+					},
+				});
+				
 				// set height and visibility of top-bar (overwrite top-bar-js)
 				setMainnavHeight()
 				
@@ -89,6 +141,17 @@ jQuery.noConflict();
 				
 				$('#toggle-topbar').css('left', $('body').width() - $('#mainnav').width() - $('#toggle-topbar').width());
 				
+				// generate foundation dropdowns
+				createDropdownClasses();
+				createTopbarBacklink();
+				$(document).foundation({
+					topbar: {
+						mobile_show_parent_link: false,
+						back_text: 'Übersicht Menü', // Define what you want your custom back text to be if custom_back_text: true
+					},
+				});
+				overwriteTopbarBackhandler();
+		
 				// show other dependencies
 				$('body').addClass('expanded');
 				
@@ -107,8 +170,8 @@ jQuery.noConflict();
 					
 				// move top-bar to active link
 				if ($('#mainnav li.moved').length == 0) {
-					$('#mainnav li.enabled a:first').each(function() {
-						$(this).click();
+					$('#mainnav li.enabled').each(function() {
+						$(this).children('a:first').click();
 					});
 				}
 			}
